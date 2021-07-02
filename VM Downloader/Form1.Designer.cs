bcdf6d24057057e320aabdef3e283f259613b388
M² <80332192+Mustafa-Mert-Akceylan@users.noreply.github.com>
2021-07-02 11:51:34 +0300

    Add files via upload

---
+++ VM Downloader/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace VM_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cikis = new System.Windows.Forms.Button();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.cikis);
+            this.panel1.Controls.Add(this.labelHeader);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(785, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // cikis
+            // 
+            this.cikis.FlatAppearance.BorderSize = 0;
+            this.cikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cikis.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikis.ForeColor = System.Drawing.Color.White;
+            this.cikis.Location = new System.Drawing.Point(721, 13);
+            this.cikis.Name = "cikis";
+            this.cikis.Size = new System.Drawing.Size(40, 40);
+            this.cikis.TabIndex = 1;
+            this.cikis.Text = "x";
+            this.cikis.UseVisualStyleBackColor = true;
+            this.cikis.Click += new System.EventHandler(this.cikis_Click);
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHeader.ForeColor = System.Drawing.Color.White;
+            this.labelHeader.Location = new System.Drawing.Point(12, 18);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(169, 30);
+            this.labelHeader.TabIndex = 0;
+            this.labelHeader.Text = "VM Downloader";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Video Url:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(120, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(590, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "İndir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDurum.ForeColor = System.Drawing.Color.White;
+            this.labelDurum.Location = new System.Drawing.Point(13, 141);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(571, 23);
+            this.labelDurum.TabIndex = 8;
+            this.labelDurum.Text = "-";
+            this.labelDurum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelDurum.Click += new System.EventHandler(this.labelDurum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(785, 184);
+            this.Controls.Add(this.labelDurum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cikis;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelDurum;
+    }
+}
+
---
+++ VM Downloader/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using VideoLibrary;
+using MediaToolkit;
+using System.Net;
+
+
+namespace VM_Downloader
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        bool FormatDurum = true;
+
+        // True ise => mp3
+        // False ise => mp4
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //await
+
+            using (FolderBrowserDialog fdb = new FolderBrowserDialog() { Description = "Lütfen Kaydetmek İstediğiniz Klasorü Seçin" })
+            {
+
+                if (fdb.ShowDialog() == DialogResult.OK)
+                {
+
+                    GetTitle();
+                    labelDurum.Text = "İndirme İşlemi Başladı. Lütfen Bekleyin.";
+                    labelDurum.ForeColor = Color.Red;
+
+                    var youtube = YouTube.Default;
+                    var video = await youtube.GetVideoAsync(textBox1.Text);
+                    File.WriteAllBytes(fdb.SelectedPath + @"\" + video.FullName, await video.GetBytesAsync());
+
+
+                    var inputFile = new MediaToolkit.Model.MediaFile { Filename = fdb.SelectedPath + @"\" + video.FullName };
+                    var outputFile = new MediaToolkit.Model.MediaFile { Filename = $"{fdb.SelectedPath + @"\" + video.FullName}.mp3" };
+
+                    using (var enging = new Engine())
+                    {
+                        enging.GetMetadata(inputFile);
+                        enging.Convert(inputFile, outputFile);
+                    }
+
+                    if (FormatDurum == true)
+                    {
+                        File.Delete(fdb.SelectedPath + @"\" + video.FullName);
+                    }
+                    else
+                    {
+                        File.Delete($"{fdb.SelectedPath + @"\" + video.FullName}.mp3");
+                    }
+                    labelHeader.Text = "VM Downloader";
+                    labelDurum.Text = "İndirme İşlemi Tamamlandı!";
+                    labelDurum.ForeColor = Color.Green;
+
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Dosya Yolunu Seçin", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        void GetTitle()
+        {
+            WebRequest istek = HttpWebRequest.Create(textBox1.Text);
+            WebResponse yanıt;
+            yanıt = istek.GetResponse();
+            StreamReader bilgiler = new StreamReader(yanıt.GetResponseStream());
+            string gelen = bilgiler.ReadToEnd();
+            int baslangic = gelen.IndexOf("<title>") + 7;
+            int bitis = gelen.Substring(baslangic).IndexOf("</title>");
+            string gelenbilgiler = gelen.Substring(baslangic, bitis);
+            labelHeader.Text = (gelenbilgiler);
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void labelDurum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
